907565d7199ac8e8c000dcfaa60635c622edd729
Vlad Protsenko <vlad@refold.io>
2024-04-25 16:15:45 +0200

    Remove unneeded .tl files

--- prepare_binaries.sh
+++ prepare_binaries.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -o errexit
+
+find bin -type f -name "*.tl" -delete
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/arm64-macos/luajit-64 bin/arm64-macos/bin/arm64-macos/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-macos/luajit-64 bin/x86_64-macos/bin/x86_64-macos/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-linux/luajit-64 bin/x86_64-linux/bin/x86_64-linux/bin
