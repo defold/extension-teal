348a46c0c7192d28aac6a5490d1c17dea0893645
Vlad Protsenko <vlad@refold.io>
2024-04-26 15:10:23 +0200

    Add linux and x86-64-macos bin zips

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Build
 
+If you modify teal executable, re-assemble the bin zips:
+```shell
+./prepare_binaries.sh
+```
+
+If you modify extension code (Java), recompile the jar before commit:
 ```shell
 ./build_plugin.sh
 ```
--- prepare_binaries.sh
+++ prepare_binaries.sh
@@ -2,13 +2,28 @@
 
 set -o errexit
 
-find bin -type f -name "*.tl" -delete
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/arm64-macos/luajit-64 bin/arm64-macos/bin/arm64-macos/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-macos/luajit-64 bin/x86_64-macos/bin/x86_64-macos/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-linux/luajit-64 bin/x86_64-linux/bin/x86_64-linux/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-win32/luajit-64.exe bin/x86_64-win32/bin/x86_64-win32/bin
+
+# assemble zips
+
 rm teal/plugins/arm64-macos.zip
 pushd bin/arm64-macos > /dev/null
 zip -r -q ../../teal/plugins/arm64-macos.zip bin
 popd > /dev/null
+
+rm teal/plugins/x86_64-linux.zip
+pushd bin/x86_64-linux > /dev/null
+zip -r -q ../../teal/plugins/x86_64-linux.zip bin
+popd > /dev/null
+
+rm teal/plugins/x86_64-macos.zip
+pushd bin/x86_64-macos > /dev/null
+zip -r -q ../../teal/plugins/x86_64-macos.zip bin
+popd > /dev/null
+
+echo "TODO: x86_64-win32"
+
 echo "Done"
