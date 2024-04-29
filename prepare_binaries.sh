efbd62235e4fa375a42463c62cc9674a4a8ee42f
Vlad Protsenko <vlad@refold.io>
2024-04-29 15:39:39 +0200

    Windows: WIP 2

--- prepare_binaries.sh
+++ prepare_binaries.sh
@@ -19,7 +19,7 @@ find bin -type f -name "*.tl" -delete
 cp cyan bin/arm64-macos/bin/arm64-macos/bin
 cp cyan bin/x86_64-macos/bin/x86_64-macos/bin
 cp cyan bin/x86_64-linux/bin/x86_64-linux/bin
-cp cyan.bat bin/x86_64-linux/bin/x86_64-win32/bin
+cp cyan.bat bin/x86_64-win32/bin/x86_64-win32/bin
 
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/arm64-macos/luajit-64 bin/arm64-macos/bin/arm64-macos/bin
 cp ../defold/com.dynamo.cr/com.dynamo.cr.bob/libexec/x86_64-macos/luajit-64 bin/x86_64-macos/bin/x86_64-macos/bin
@@ -43,6 +43,7 @@ pushd bin/x86_64-macos > /dev/null
 zip -r -q ../../teal/plugins/x86_64-macos.zip bin
 popd > /dev/null
 
+rm teal/plugins/x86_64-win32.zip
 pushd bin/x86_64-win32 > /dev/null
 zip -r -q ../../teal/plugins/x86_64-win32.zip bin
 popd > /dev/null
