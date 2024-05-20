d3a503015217a70f58969d5da830ec18cf57cb55
Mats Gisselson <matgis@users.noreply.github.com>
2024-05-20 12:16:58 +0200

    Clean out expected output files from the output directory before transpiling (#1)

--- src/main/java/com/defold/extension/pipeline/TealTranspiler.java
+++ src/main/java/com/defold/extension/pipeline/TealTranspiler.java
@@ -1,11 +1,15 @@
 package com.defold.extension.pipeline;
 
 import com.dynamo.bob.Platform;
+import org.apache.commons.io.FilenameUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -42,6 +46,7 @@ public class TealTranspiler implements ILuaTranspiler {
 
     @Override
     public List<Issue> transpile(File pluginDir, File sourceDir, File outputDir) throws Exception {
+        cleanOutput(sourceDir, outputDir);
         Platform platform = Platform.getHostPlatform();
         String exeSuffix = "win32".equals(platform.getOs()) ? ".bat" : "";
         String cmd = new File(pluginDir, "teal/plugins/bin/" + platform.getPair() + "/bin/cyan" + exeSuffix).toString();
@@ -96,5 +101,77 @@ public class TealTranspiler implements ILuaTranspiler {
                     String.join("\n", lines)));
         }
         return result;
+    }
+
+    private static void cleanOutput(File sourceDir, File outputDir) throws IOException {
+        if (!outputDir.isDirectory()) {
+            return;
+        }
+
+        // Cyan will only re-transpile a source file if it is newer than the
+        // output file. This is an issue for us due to how the editor prepares
+        // source files for compilation. As a workaround, we will clean the
+        // output directory before invoking Cyan. However, Cyan can be
+        // configured to selectively prune files from the output directory, so
+        // even though we supply the --prune flag, simply emptying the output
+        // directory is too intrusive. Instead, we specifically remove only the
+        // expected output files from the output directory.
+        // See https://github.com/defold/defold/issues/8928 for details.
+        Path sourceDirPath = sourceDir.toPath().toRealPath(LinkOption.NOFOLLOW_LINKS);
+        Path outputDirPath = outputDir.toPath().toRealPath(LinkOption.NOFOLLOW_LINKS);
+        List<Path> expectedOutputFilePaths = getExpectedOutputFilePaths(sourceDirPath, outputDirPath);
+
+        for (Path path : expectedOutputFilePaths) {
+            if (Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)) {
+                Files.delete(path);
+            }
+        }
+    }
+
+    private static List<Path> getExpectedOutputFilePaths(Path sourceDirPath, Path outputDirPath) throws IOException {
+        if (sourceDirPath.equals(outputDirPath)) {
+            // Cyan does not write any .lua files in this case.
+            return List.of();
+        }
+
+        List<Path> tealSourceFilePaths = getTealSourceFilePaths(sourceDirPath);
+        return getLuaOutputFilePaths(tealSourceFilePaths, sourceDirPath, outputDirPath);
+    }
+
+    private static List<Path> getTealSourceFilePaths(Path sourceDirPath) throws IOException {
+        PathMatcher tealSourceFilePathMatcher = FileSystems.getDefault().getPathMatcher("glob:**.tl");
+        Path buildDirPath = sourceDirPath.resolve("build");
+        ArrayList<Path> tealSourceFilePaths = new ArrayList<>(128);
+
+        Files.walkFileTree(sourceDirPath, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dirPath, BasicFileAttributes attributes) throws IOException {
+                boolean skip = dirPath.equals(buildDirPath) || Files.isHidden(dirPath);
+                return skip ? FileVisitResult.SKIP_SUBTREE : FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path filePath, BasicFileAttributes attributes) throws IOException {
+                if (tealSourceFilePathMatcher.matches(filePath) && Files.isRegularFile(filePath, LinkOption.NOFOLLOW_LINKS) && !Files.isHidden(filePath)) {
+                    tealSourceFilePaths.add(filePath);
+                }
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return tealSourceFilePaths;
+    }
+
+    private static List<Path> getLuaOutputFilePaths(List<Path> tealSourceFilePaths, Path sourceDirPath, Path outputDirPath) {
+        return tealSourceFilePaths.stream().map(path -> getLuaOutputFilePath(path, sourceDirPath, outputDirPath)).toList();
+    }
+
+    private static Path getLuaOutputFilePath(Path tealSourceFilePath, Path sourceDirPath, Path outputDirPath) {
+        String tealSourceFileName = tealSourceFilePath.getFileName().toString();
+        String luaOutputFileName = FilenameUtils.removeExtension(tealSourceFileName).concat(".lua");
+        Path relativeTealSourceFilePath = sourceDirPath.relativize(tealSourceFilePath);
+        Path relativeLuaOutputFilePath = relativeTealSourceFilePath.resolveSibling(luaOutputFileName);
+        return outputDirPath.resolve(relativeLuaOutputFilePath);
     }
 }
